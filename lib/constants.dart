d287f283d6fb42b3a024ee28a3c7b80db6fc5bf6
shakthi <shakthi.narayan@superprocure.com>
2025-10-05 17:27:32 +0530

    api end points

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 class Constants {
   static const String imageBBApiKey = 'ce930c3338444c9f3f12f003914c8de0';
   static const String sambaApiKey = 'e6502739-b55f-4d52-91a5-9157f2b84a37';
-  static const String endPoint = 'https://chatai.free.beeceptor.com/';
+  static const String endPoint =
+      'https://e126a927-65f6-47ba-ab8e-fef41082d59b.mock.pstmn.io/';
   static const jobEndPoint =
       'https://b91b1769-22fa-4230-b610-b2cc51351e9b.mock.pstmn.io/';
   static const sambaEndPoint = 'https://api.sambanova.ai/v1';
